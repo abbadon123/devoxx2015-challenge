d86162357bbf386bb67440df820e60ce73964ade
Mateusz Kanarek <Mateusz.Kanarek.ctr@sabre.com>
2015-08-18 11:15:01 +0200

    OfferService implementation; to github

--- src/main/java/pl/allegro/promo/devoxx2015/application/OfferService.java
+++ src/main/java/pl/allegro/promo/devoxx2015/application/OfferService.java
@@ -1,15 +1,18 @@
 package pl.allegro.promo.devoxx2015.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.allegro.promo.devoxx2015.domain.Offer;
 import pl.allegro.promo.devoxx2015.domain.OfferRepository;
 import pl.allegro.promo.devoxx2015.domain.PhotoScoreSource;
 
-import java.util.List;
-
 @Component
 public class OfferService {
+
+    private static final double MINIMAL_ACCEPTANCE_SCORE = 0.7;
 
     private final OfferRepository offerRepository;
     private final PhotoScoreSource photoScoreSource;
@@ -21,10 +24,42 @@ public class OfferService {
     }
 
     public void processOffers(List<OfferPublishedEvent> events) {
-        // TODO save offers with pretty photos
+
+        Iterable<Offer> offers = events.stream()
+                .map(this::findOffer)
+                .filter(this::isPretty)
+                .collect(Collectors.toList());
+
+        offerRepository.save(offers);
+    }
+
+    private Offer findOffer(OfferPublishedEvent e) {
+        double score = getScore(e.getPhotoUrl());
+        return new Offer(e.getId(), e.getTitle(), e.getPhotoUrl(), score);
+    }
+
+    private boolean isPretty(Offer offer) {
+        return  offer.getPhotoScore() >= MINIMAL_ACCEPTANCE_SCORE;
+    }
+
+    private double getScore(String photoUrl) {
+        try {
+            return  photoScoreSource.getScore(photoUrl);
+        } catch (Exception ex) {
+            return MINIMAL_ACCEPTANCE_SCORE;
+        }
     }
 
     public List<Offer> getOffers() {
-        return offerRepository.findAll(); // TODO some sorting?
+        return offerRepository.findAll()
+                .stream()
+                .sorted(OfferService::byPhotoScoreDescending)
+                .collect(Collectors.toList());
     }
+
+    public static int byPhotoScoreDescending(Offer o1, Offer o2) {
+        return Double.compare(o2.getPhotoScore(), o1.getPhotoScore());
+    }
+
+
 }
